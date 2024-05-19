22216f178c0fa41cf3ac2eb240b5ebc1f6ba9850
nomfodm <nomfodm@gmail.com>
2024-05-19 17:28:45 +0300

    112497 - Из инфиксной в постфиксную

---
+++ 112497.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool is_number(char c) {
+    return c >= '0' && c <= '9';
+}
+
+int main () {
+    string s;
+    getline(cin, s);
+
+
+    vector<string> ans;
+    stack<string> st;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (is_number(s[i])) {
+            if (i != 0 && is_number(s[i - 1])) {
+
+                ans[ans.size() - 1] += s[i];
+
+            } else {
+                
+                ans.push_back(""); ans[ans.size() - 1] += s[i];
+
+            }
+
+        } else {
+
+            if (st.empty()) {
+
+                st.push(""); st.top() += s[i];
+
+            } else if ((s[i] == '*' || s[i] == '/') && 
+            (st.top() == "+" || st.top() == "-")) {
+
+                st.push(""); st.top() += s[i];
+
+            } else {
+
+                if (s[i] == '+' || s[i] == '-') {
+
+                    while (!st.empty()) {
+                        ans.push_back(""); ans[ans.size() - 1] += st.top();
+
+                        st.pop();
+                    }
+
+                    st.push(""); st.top() += s[i];
+
+                } else {
+
+                    while (!st.empty() && st.top() != "+" && st.top() != "-") {
+                        ans.push_back(""); ans[ans.size() - 1] += st.top();
+
+                        st.pop();
+                    }
+
+                    st.push(""); st.top() += s[i];
+
+                }
+            }
+        }
+    }
+    
+
+    for (string s : ans) {
+        cout << s << " ";
+    }
+
+    while(!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+}
